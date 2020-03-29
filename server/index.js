89f57852f49b6deb9ca25807672ae269e812ed7d
TonyV <tvester1487@gmail.com>
2020-03-29 10:02:37 -0500

    add user/pass

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(users);
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/meetingsdb");
 var uri =
-  "mongodb://<dbuser>:<dbpassword>@ds347917.mlab.com:47917/heroku_v1q1k4v0";
+  "mongodb://<admin>:<password1>@ds347917.mlab.com:47917/heroku_v1q1k4v0";
 
 var options = {
   server: {
