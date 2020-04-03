bbcd5efd4f874d7fb74affcf9e15f0cd3542e886
TonyV <tvester1487@gmail.com>
2020-04-02 20:19:39 -0500

    move / to routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,4 +11,8 @@ router.use("/api", apiRoutes);
 //   res.sendFile(path.join(__dirname, "../../client/public/index.html"));
 // });
 
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 module.exports = router;
