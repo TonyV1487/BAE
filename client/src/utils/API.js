bbf13d66be394c5b4f04861b34744e94c6f01d43
TonyV <tvester1487@gmail.com>
2020-04-03 20:24:24 -0500

    server changed to axios

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,52 +1,52 @@
 import axios from "axios";
 // const mongoose = require("mongoose");
 
-const server = axios.create({ baseURL: "http://localhost:3001/" });
+// const server = axios.create({ baseURL: "http://localhost:3001/" });
 
 export default {
   /* User Routes */
   // Gets all users
-  getAllUsers: function () {
-    return server.get("/api/user");
+  getAllUsers: function() {
+    return axios.get("/api/user");
   },
   // Gets the user with the given id
-  getUser: function (id) {
-    return server.get(`/api/user/${id}`);
+  getUser: function(id) {
+    return axios.get(`/api/user/${id}`);
   },
   // Deletes the user with the given id
-  deleteUser: function (id) {
-    return server.delete(`/api/user/${id}`);
+  deleteUser: function(id) {
+    return axios.delete(`/api/user/${id}`);
   },
   // Saves a user to the database
-  createUser: function (userData) {
-    return server.post("/api/user", userData);
+  createUser: function(userData) {
+    return axios.post("/api/user", userData);
   },
 
   /* Meetings Routes */
   // Gets all meetings
-  getAllMeetings: function () {
-    return server.get("/api/meeting");
+  getAllMeetings: function() {
+    return axios.get("/api/meeting");
   },
   // Gets the meetings with the given id
-  getMeeting: function (id) {
+  getMeeting: function(id) {
     // const objectId = new ObjectId(id);
     // console.log(objectId);
-    return server.get(`/api/meeting/${id}`);
+    return axios.get(`/api/meeting/${id}`);
   },
   // Deletes the meetings with the given id
-  deleteMeeting: function (id) {
-    return server.delete(`/api/meeting/${id}`);
+  deleteMeeting: function(id) {
+    return axios.delete(`/api/meeting/${id}`);
   },
   // Saves a meetings to the database
-  createMeeting: function (meetingData) {
-    return server.post("/api/meeting", meetingData);
+  createMeeting: function(meetingData) {
+    return axios.post("/api/meeting", meetingData);
   },
 
-  updateMeeting: function (id, meetingData) {
-    return server.put(`/api/meeting/${id}`, meetingData);
+  updateMeeting: function(id, meetingData) {
+    return axios.put(`/api/meeting/${id}`, meetingData);
   },
 
-  checkUser: function (user) {
-    return server.post("/api/user/login", user);
+  checkUser: function(user) {
+    return axios.post("/api/user/login", user);
   }
 };
